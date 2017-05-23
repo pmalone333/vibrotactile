bf7006bd21c69e85717fbca76786854999982132
VT Laptop <levan.bokeria@gmail.com>
2017-05-23 17:40:15 -0400

    VT OB screen task

---
+++ 04_MRI/05_Aim2/playOB.m
@@ -0,0 +1,14 @@
+load('oddball_aba2_whiteNoiseAmplifiedC.mat')
+
+tm = stim{1}{1};
+ch = stim{1}{2};
+
+stimGenPTB('open','COM1')
+
+for i=1:5
+    stimGenPTB('load',ch,tm);
+    stimGenPTB('start');
+    WaitSecs(2);
+end
+
+stimGenPTB('close','COM1')
