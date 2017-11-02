92dcd1a00a0d71adcc4f8f8b5aa224137f5865ee
VT Laptop <levan.bokeria@gmail.com>
2017-11-02 13:23:30 -0400

    update training data

--- 13_FB/testStim.m
+++ 13_FB/testStim.m
@@ -1,5 +1,5 @@
-piezoDriver32('open','COM1');
-load('TSfullsweep_48000.mat');
+piezoDriver32('open','COM5');
+load('singlehigh16_stim.mat');
 piezoDriver32('load',uint16(t),uint16(s));
 piezoDriver32('start');
 piezoDriver32('close');
